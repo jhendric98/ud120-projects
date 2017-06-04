cb6035ded7a11360b948a15bcc8392b5e1861a56
jhendric98 <jhendric98@gmail.com>
2017-06-03 23:01:13 -0400

    complete work on choose your own algorithm

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -31,7 +31,24 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import accuracy_score
+from time import time
 
+
+clf = AdaBoostClassifier(n_estimators=100)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
+
+accuracy = accuracy_score(labels_test, pred)
+
+print accuracy
 
 
 
