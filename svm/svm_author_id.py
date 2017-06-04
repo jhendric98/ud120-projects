61c128f56d84231e6de37cba77594e46868454aa
jhendric98 <jhendric98@gmail.com>
2017-06-03 23:14:24 -0400

    complete work on questions and naive bayes

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,27 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+import numpy as np
+from sklearn.svm import SVC
+#clf = SVC(kernel="linear")
+clf = SVC(C=10000.0, kernel="rbf")
+
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
+
+total = np.count_nonzero(pred)
+print total
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+
+print acc
 
 #########################################################
 
